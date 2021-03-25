59b7d7b85e6f9c799c8dba7fe8b9dce10071ea96
waltervetrivel737 <waltervetrivel737@gmail.com>
2021-03-25 12:53:15 +0530

    new

--- src/test/java/org/samp/Gitproj.java
+++ src/test/java/org/samp/Gitproj.java
@@ -4,6 +4,9 @@ public class Gitproj {
 	public static void main(String[] args) {
 		System.out.println("walter");
 		System.out.println("walter2");
+		System.out.println("w3");
+		System.out.println("w4");
+		
 	}
 
 }
